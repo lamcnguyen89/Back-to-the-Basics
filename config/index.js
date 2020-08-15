c1a6b732ae469726e555a6c245060f6c2ea40f47
Lam Nguyen <lamcnguyen89@gmail.com>
2020-08-15 02:49:33 -0400

    Reverted some code back so that we would have a working boilerplate for our project

--- config/index.js
+++ config/index.js
@@ -1,24 +1,5 @@
 // Credentials for jwt Secret and Database
-// module.exports = {
-//     MONGO_URI: process.env.MONGODB_URI || process.env.DATABASE_INFO,
-//     jwtSecret: process.env.JWT_SECRET
-// }
-
-
-
-var Sequelize = require("sequelize");
-
-// Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
-var sequelize = new Sequelize("sequelize_library", "root", "", {
-  host: "localhost",
-  port: 3306,
-  dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
-  }
-});
-
-// Exports the connection for other files to use
-module.exports = sequelize;
+module.exports = {
+    MONGO_URI: process.env.MONGODB_URI || process.env.DATABASE_INFO,
+    jwtSecret: process.env.JWT_SECRET
+}
--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const path = require("path");
-// const mongoose = require("mongoose");
-const mysql = require("mysql2");
+const mongoose = require("mongoose");
 require("dotenv").config();
 const config = require("./config");
 const routes = require("./routes");
-const db = require("./models");
+
 const app = express();
 
 // middleware to parse data
@@ -17,26 +16,18 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "./client/build")))
 };
 
-// check for "production" enviroment and set port
-const PORT = process.env.PORT || 3006;
-
-// connect to DB 
-db.sequelize.sync().then(() => {
-    app.listen(PORT, () => {
-      console.log(`app listening on: http://localhost:${PORT}`);
-    });
-  });
-
-// mongoose.connect(config.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: true })
-//     .then(() => console.log(`Mongo DB Succesfully Connected`))
-//     .catch(err => console.log(err));
+// connect to Mongo DB 
+mongoose.connect(config.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: true })
+    .then(() => console.log(`Mongo DB Succesfully Connected`))
+    .catch(err => console.log(err));
 
 // use routes
 app.use(routes);
 
-
+// check for "production" enviroment and set port
+const PORT = process.env.PORT || 3001;
 
 // start server
-// app.listen(PORT, () => {
-//     console.log(`App listening on port: ${PORT}`);
-// })
+app.listen(PORT, () => {
+    console.log(`App listening on port: ${PORT}`);
+})
