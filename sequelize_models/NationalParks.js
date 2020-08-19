cb2d6b5793ba7f990642c63f022ba60d9f3f200d
Lam Nguyen <lamcnguyen89@gmail.com>
2020-08-18 22:14:00 -0400

    Our code is working but we somehow have to get sequelize to work

--- sequelize_models/NationalParks.js
+++ sequelize_models/NationalParks.js
@@ -1,20 +1,24 @@
 module.exports = function (sequelize, DataTypes) {
     var NationalParks = sequelize.define("national_parks", {
       site_name: {
-        type: DataTypes.STRING,
+        type: Sequelize.STRING,
+        // type: DataTypes.STRING,
         allowNull: false,
       },
       address: {
-        type: DataTypes.STRING,
+        type: Sequelize.STRING,
+        // type: DataTypes.STRING,
         allowNull: false,
       },
   
       lat: {
-        type: DataTypes.FLOAT,
+        type: Sequelize.FLOAT,
+        // type: DataTypes.FLOAT,
       },
   
       long: {
-        type: DataTypes.FLOAT,
+        type: Sequelize.FLOAT,
+        // type: DataTypes.FLOAT,
       },
     });
     return NationalParks;
--- sequelize_models/StateParks.js
+++ sequelize_models/StateParks.js
@@ -1,20 +1,24 @@
 module.exports = function (sequelize, DataTypes) {
     var StateParks = sequelize.define("state_parks", {
       site_name: {
-        type: DataTypes.STRING,
+        type: Sequelize.STRING,
+        // type: DataTypes.STRING,
         allowNull: false,
       },
       address: {
-        type: DataTypes.STRING,
+        type: Sequelize.STRING,
+        // type: DataTypes.STRING,
         allowNull: false,
       },
   
       lat: {
-        type: DataTypes.FLOAT,
+        type: Sequelize.FLOAT,
+        // type: DataTypes.FLOAT,
       },
   
       long: {
-        type: DataTypes.FLOAT,
+        type: Sequelize.FLOAT,
+        // type: DataTypes.FLOAT,
       },
     });
     return StateParks;
