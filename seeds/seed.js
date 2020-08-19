9c83c7676aeaa5f3f3decd2f14b04c63446f609c
Lam Nguyen <lamcnguyen89@gmail.com>
2020-08-19 12:45:22 -0400

    Added comments to the seeds

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,4 +1,6 @@
-const OutdoorStores = require("../models/OutdoorStores")
+// Import CSV files into MongoDB. 
+// To import, in the terminal, go to where the CSV files are located and run each line individually that starts with mongoimport
+// Source: https://kb.objectrocket.com/mongo-db/how-to-import-a-csv-into-mongodb-327
 
 
 mongoimport --type csv -d backbasicsdb -c beaches --headerline --drop Beaches.csv;
